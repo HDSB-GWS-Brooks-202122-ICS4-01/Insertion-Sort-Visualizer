86385c7199628aed17874dcdd0390a74f1fe1235
Mimo604 <56412271+SelimAhmed@users.noreply.github.com>
2021-11-26 21:41:55 -0500

    Application is complete
    The program is complete, many, many changes. I haven't added man new thing but all the functionalities fully work now. The app looks quite nice if I do say so myself.
    
    As always I tripled checked that all methods are documented so if one isn't, then someone else must've deleted it.

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -1,5 +1,13 @@
+/* *****************************************************************************
+ *  Name:    Selim Abdelwahab
+ *
+ *  Description: This class is the Controllor for the home screen. Responsible for minor calculations and switching to the next scene.
+ *
+ *  Written:       23/11/2021
+ *  Last updated:  26/11/2021
+ **************************************************************************** */
+
 import java.io.IOException;
-import java.util.Random;
 
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
@@ -8,11 +16,10 @@ import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
 public class HomeController {
@@ -20,42 +27,64 @@ public class HomeController {
    StackPane sp_root;
 
    @FXML
-   VBox vb_container;
+   HBox hb_container;
 
    @FXML
-   Button btn_startSort;
+   Slider slider_nor;
 
    @FXML
-   void switchToSort() throws IOException {
+   Button btn_insertionSort;
+
+   @FXML
+   /**
+    * This method will call switch to sort with the correct algorithim name.
+    * 
+    * @throws IOException Will throw an error if the FXML file is unable to load.
+    */
+   void loadInsertionSort() throws IOException {
+      switchToSort("Insertion Sort");
+   }
+
+   /**
+    * This method will play a scene animation, while also creating an array and
+    * switching to the next scene.
+    * 
+    * @param name Name of the algorithim
+    * @throws IOException Will throw an error if the FXML file is unable to load.
+    */
+   void switchToSort(String name) throws IOException {
       Parent nextScene = App.loadFXML("sort");
       sp_root.getChildren().add(nextScene);
       nextScene.translateYProperty().set(sp_root.getScene().getHeight());
 
       Pane grid = (Pane) sp_root.getScene().lookup("#grid");
 
-      // Create a timeline instance
-      Timeline timeline = new Timeline();
-      // Create a keyValue. We need to slide in -- We gradually decrement Y value to
-      // Zero
-      KeyValue kv = new KeyValue(nextScene.translateYProperty(), 0, Interpolator.EASE_OUT);
-      // Create keyframe of 1s with keyvalue kv
-      KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
-      // Add frame to timeline
-      timeline.getKeyFrames().add(kf);
+      // Create a timeline
+      Timeline timeline = new Timeline(
+            new KeyFrame(Duration.seconds(1), new KeyValue(nextScene.translateYProperty(), 0, Interpolator.EASE_BOTH)),
+            new KeyFrame(Duration.seconds(1),
+                  new KeyValue(hb_container.translateYProperty(), -sp_root.getHeight(), Interpolator.EASE_BOTH)));
+      
+      // Play ani
+      timeline.play();
 
-      // Start animation
+      if (slider_nor.getValue() < 2)
+         slider_nor.setValue(2);
+
       timeline.setOnFinished((e) -> {
-         sp_root.getChildren().remove(vb_container);
+         // Remove containers
+         sp_root.getChildren().remove(hb_container);
          sp_root.getChildren().remove(nextScene);
 
-         double[] array = new double[grid.getChildren().size()];
+         int width = (int) Math.round(grid.getWidth() / slider_nor.getValue());
+         int size = (int) Math.round(slider_nor.getValue());
 
-         for (int i = 0; i < array.length; i++) {
-            array[i] = App.RAND.nextDouble() * grid.getHeight();
-         }
+         App.setWidth(width);
 
-         App.setArray(array);
+         App.setArray(App.generateArray(size, grid.getHeight()));
+         App.setAlgorithm(name);
 
+         // Sneakily set the scen ;)
          try {
             App.setRoot("sort");
          } catch (IOException err) {
--- src/main/java/SortController.java
+++ src/main/java/SortController.java
@@ -1,47 +1,86 @@
+/* *****************************************************************************
+ *  Name:    Selim Abdelwahab
+ *
+ *  Description: This class is the Controller for the Sort scene. All the beauty happens in this class, sorting, animations, UI, pretty much the whole program.
+ *
+ *  Written:       23/11/2021
+ *  Last updated:  26/11/2021
+ **************************************************************************** */
+
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import javafx.animation.FadeTransition;
-import javafx.animation.ParallelTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
-import javafx.animation.Animation.Status;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.layout.GridPane;
+import javafx.scene.Parent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
+/**
+ * Sort controller class
+ */
 public class SortController {
    private Rectangle[] array;
    private Rectangle[] defaultArray;
-   private Stage stage;
-   private final Random RAND = new Random();
    private boolean oppositeDirection = false;
-   private boolean playAuto = false;
 
-   private final Timer TIMER = new Timer();
-   private final TimerTask AUTO = new TimerTask() {
-      public void run() {
-         if (playAuto)
-            sort(true, oppositeDirection);
-      }
-   };
+   private int iSort = 0, jSort = 1;
+   private double inactiveOpacity = 0.2;
 
-   private int iSort = 0, jSort = 0;
+   private Rectangle previous = null;
+
+   private double autoTimeInterval = 1000;
+   private boolean sortComplete = false;
+   private boolean playAni = false;
+
+   private int reminaingComparisons;
+   private int defaultRemainingComparisons;
+
+   @FXML
+   StackPane sp_root;
+
+   @FXML
+   HBox hb_container;
+
+   @FXML
+   Slider slider_speed;
+
+   @FXML
+   ImageView img_iArrow, img_jArrow;
 
    @FXML
    Pane grid;
 
    @FXML
-   private void switchToPrimary() throws IOException {
-      App.setRoot("primary");
-   }
+   // Label name of algorithm //Label speed of animation // Label number of
+   // rectangles // Label remaining
+   // comparisons // Label main
+   // rectangle // Label sub rectangle // Label positive comparison // Lable sort
+   // complete
+   Label lbl_title, lbl_soa, lbl_nor, lbl_rc, lbl_mr, lbl_sr, lbl_pc, lbl_sc;
+
+   @FXML
+   CheckBox cb_displayArrows;
+
+   Timeline autoSort;
 
    @FXML
    /**
@@ -51,118 +90,264 @@ public class SortController {
       double[] arrayHeights = App.getArray();
 
       if (arrayHeights != null) {
-         stage = App.getStage();
-
          array = new Rectangle[arrayHeights.length];
 
-         // for (int i = array.length - 1; i >= 0; i--) {
-         // Rectangle rect = (Rectangle) grid.getChildren().get(array.length - i - 1);
+         reminaingComparisons = 0;
 
-         // double height = arrayHeights[i];
-
-         // if (height < 0.2)
-         // height += 0.3;
-
-         // int rectHeight = (int) height;
-
-         // rect.setFill(new Color(App.RAND.nextDouble(), App.RAND.nextDouble(),
-         // App.RAND.nextDouble(), 1.0));
-         // rect.setHeight(rectHeight);
-         // rect.setOpacity(0);
-
-         // FadeTransition fiTransition = new FadeTransition(Duration.seconds(3), rect);
-         // fiTransition.setFromValue(0.0);
-         // fiTransition.setToValue(1.0);
-         // fiTransition.play();
-
-         // array[array.length - i - 1] = rect;
-         // }
-
+         // Loop through the array of heights
          for (int i = 0; i < array.length; i++) {
-            Rectangle rect = (Rectangle) grid.getChildren().get(i);
+            reminaingComparisons += i;
+
+            // Create a new rectangle and define a few properties
+            Rectangle rect = new Rectangle(App.getRectWidth(), 0);
+            rect.setStroke(Color.BLACK);
+            rect.setStrokeWidth(App.getRectWidth() / 5);
+
+            // Add the rectangle to the scene
+            grid.getChildren().add(rect);
+
+            // Adjust it's x position
+            rect.setX(App.getRectWidth() * i);
 
             double height = arrayHeights[i];
 
-            if (height < 0.2)
-               height += 0.3;
+            // Adjust the height if neccessary
+            if (height / grid.getMinHeight() < 0.1)
+               height *= 2;
 
-            int rectHeight = (int) height;
+            // rect.setFill(new Color(App.RAND.nextDouble(), App.RAND.nextDouble(),
+            // App.RAND.nextDouble(), 1.0));
 
-            rect.setFill(new Color(App.RAND.nextDouble(), App.RAND.nextDouble(), App.RAND.nextDouble(), 1.0));
-            rect.setHeight(rectHeight);
+            // Set color, height and opacity
+            rect.setFill(Color.GREY);
+            rect.setHeight(height);
             rect.setOpacity(0);
 
+            // Subtle fade in animation
             FadeTransition fiTransition = new FadeTransition(Duration.seconds(1.5), rect);
             fiTransition.setFromValue(0.0);
-            fiTransition.setToValue(1.0);
+            fiTransition.setToValue((i > 1) ? inactiveOpacity : 1.0);
             fiTransition.play();
 
+            // Finally assing the rectangle a position in the array
             array[i] = rect;
          }
 
+         // Copy the array over to another one should the user wish to reset
          defaultArray = Arrays.copyOf(array, array.length);
 
-         stage.widthProperty().addListener((obs, oldVal, newVal) -> {
-            for (Rectangle r : array) {
-               double scale = (double) newVal / (double) stage.getMinWidth();
-               r.setScaleX(scale);
+         defaultRemainingComparisons = reminaingComparisons;
+
+         // Notify the user with number of rectangles
+         lbl_nor.setText("Number Of Rectangles: " + array.length);
+
+         // Notify user with remianing comparisons
+         lbl_rc.setText("Remianing Comparisons: " + reminaingComparisons);
+
+         // Event handler to check if the speed slider is changed, if so the speed of the
+         // auto animation will change.
+         slider_speed.valueChangingProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean wasChanging,
+                  Boolean changing) {
+               updateAutoSort();
+            }
+         });
+         slider_speed.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+               updateAutoSort();
             }
          });
 
-         stage.heightProperty().addListener((obs, oldVal, newVal) -> {
-            for (Rectangle r : array) {
-               double scale = (double) newVal / (double) stage.getMinHeight();
-               r.setScaleY(scale);
+         cb_displayArrows.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean wasChanging,
+                  Boolean changing) {
+               if (cb_displayArrows.isSelected()) {
+                  img_iArrow.setVisible(true);
+                  img_jArrow.setVisible(true);
+
+                  Timeline ani = new Timeline();
+                  KeyFrame frame1 = new KeyFrame(Duration.seconds(0.4),
+                        new KeyValue(img_iArrow.translateYProperty(), 0));
+                  KeyFrame frame2 = new KeyFrame(Duration.seconds(0.4),
+                        new KeyValue(img_jArrow.translateYProperty(), 0));
+
+                  ani.getKeyFrames().addAll(frame1, frame2);
+
+                  ani.play();
+               } else {
+                  Timeline ani = new Timeline();
+                  KeyFrame frame1 = new KeyFrame(Duration.seconds(0.4),
+                        new KeyValue(img_iArrow.translateYProperty(), -1000));
+                  KeyFrame frame2 = new KeyFrame(Duration.seconds(0.4),
+                        new KeyValue(img_jArrow.translateYProperty(), -1000));
+
+                  ani.getKeyFrames().addAll(frame1, frame2);
+
+                  ani.setOnFinished((e) -> {
+                     img_iArrow.setVisible(false);
+                     img_jArrow.setVisible(false);
+                  });
+
+                  ani.play();
+               }
             }
          });
+
+         img_iArrow.setX(-img_iArrow.getFitWidth() / 3);
+         img_jArrow.setX(-img_iArrow.getFitWidth() / 3);
+
+         lbl_title.setText("Algorithm: " + App.getAlgorithm().toUpperCase());
+      }
+   }
+
+   /**
+    * This method will re define the autoSort timeline as the user changes the
+    * duration.
+    */
+   private void updateAutoSort() {
+      resetAnimation();
+      int speed = (int) ((slider_speed.getMax() - slider_speed.getValue() + 0.01) * 1000);
+      autoTimeInterval = speed;
+
+      autoSort = new Timeline(new KeyFrame(Duration.millis(speed), new EventHandler<ActionEvent>() {
+         @Override
+         public void handle(ActionEvent ae) {
+            // Call the sort method.
+            if (playAni)
+               sort(oppositeDirection, false);
+         }
+      }));
+
+      // Set cyle to infinite
+      autoSort.setCycleCount(Timeline.INDEFINITE);
+
+      autoSort.play();
+
+      if (speed == 1000)
+         lbl_soa.setText("Speed: 1s");
+      else
+         lbl_soa.setText("Speed: " + speed + "ms");
+   }
+
+   /**
+    * This method will reset the auto play animation
+    */
+   private void resetAnimation() {
+      if (autoSort != null) {
+         autoSort.pause();
+         autoSort.stop();
+         autoSort.getKeyFrames().clear();
+         autoSort.setCycleCount(0);
+         autoSort.setRate(0);
+
+         autoSort = null;
       }
    }
 
    /**
     * This method will sort the array in a manner that corrosponds with the
     * paramateres
+    * 
+    * @param isReveresed Direction of sort
+    * @param fullSort    Sort all at once
     */
-   public void sort(boolean isStep, boolean isReveresed) {
-      System.out.println(true);
+   public void sort(boolean isReveresed, boolean fullSort) {
+      // Sort is already complete -> exit function
+      if (sortComplete)
+         return;
 
-      if (iSort == jSort && jSort == array.length - 1) {
-         //TODO: Tell user sorting is complete
+      // Sort is comeplete do last bit of animation
+      if (iSort > array.length || jSort > array.length - 1) {
+         for (Rectangle rect : array) {
+            rect.setOpacity(1.0);
+
+            rect.setFill(Color.TURQUOISE);
+         }
+
+         if (!sortComplete) {
+            img_iArrow.setX(img_jArrow.getX());
+            lbl_sc.setText("Sort Complete: True");
+            sortComplete = true;
+         }
          return;
       }
 
-      if (isStep) {
-         Rectangle one = array[iSort];
-         Rectangle two = array[jSort];
+      lbl_mr.setText("Main Rectangle: " + iSort);
+      lbl_sr.setText("Sub Rectangle: " + jSort);
 
-         if ((isReveresed && two.getHeight() > one.getHeight()) || (!isReveresed && two.getHeight() < one.getHeight())) {
-            TranslateTransition ttOne = new TranslateTransition(Duration.seconds(1), one);
-            double oneX = one.getX();
-            double twoX = two.getX();
+      boolean sorted = false;
 
-            // TranslateTransition ttTwo = new TranslateTransition(Duration.seconds(1),
-            // two);
+      // Fetch the current comparisons
+      Rectangle one = array[iSort];
+      Rectangle two = array[jSort];
 
-            // ttOne.setFromX(-400);
-            // ttOne.setToX(twoX);
-            // ttOne.setToY(100);
+      // Reset opacity of previous rectangle
+      if (previous != null)
+         previous.setOpacity(inactiveOpacity);
 
-            // // ttTwo.setFromX(400);
-            // // ttTwo.setToX(oneX);
-            // // ttTwo.setToY(100);
+      previous = two;
 
-            one.setX(twoX);
-            two.setX(oneX);
+      // Set opcatity of current rectangles
+      one.setOpacity(1.0);
+      two.setOpacity(1.0);
 
-            array[iSort] = two;
-            array[jSort] = one;
-         }
+      if (!fullSort) {
+         // Set x positions of arrows
+         img_iArrow.setX(one.getX() - img_iArrow.getFitWidth() / 3);
+
+         TranslateTransition jArrowAni = new TranslateTransition(Duration.millis(autoTimeInterval), img_jArrow);
+
+         jArrowAni.setToX(App.getRectWidth() * jSort - img_jArrow.getFitHeight());
+         jArrowAni.play();
+      }
+      // img_jArrow.setX(two.getX());
+
+      // Check comparison
+      if ((isReveresed && two.getHeight() > one.getHeight()) || (!isReveresed && two.getHeight() < one.getHeight())) {
+         double oneX = one.getX();
+         double twoX = two.getX();
+
+         // Change positions
+         one.setX(twoX);
+         two.setX(oneX);
+
+         array[iSort] = two;
+         array[jSort] = one;
+
+         sorted = true;
       }
 
-      if (jSort >= array.length - 1) {
-         iSort++;
-         jSort = iSort;
+      // Set opacity
+      if (sorted) {
+         lbl_pc.setText("Positive Comparison: True");
+         two.setOpacity(1.0);
+
+         previous = one;
       } else
+         lbl_pc.setText("Positive Comparison: False");
+
+      // Sorted so set color to green
+      if (jSort >= array.length - 1) {
+         if (sorted) {
+            two.setFill(Color.GREEN);
+            two.setOpacity(1.0);
+         } else {
+            one.setFill(Color.GREEN);
+            one.setOpacity(1.0);
+         }
+
+         iSort++;
+         jSort = iSort + 1;
+
+      } else {
+         // iterate to the next index.
          jSort++;
+
+      }
+
+      reminaingComparisons--;
+
+      lbl_rc.setText("Remaining Comparisons: " + reminaingComparisons);
    }
 
    /**
@@ -170,8 +355,11 @@ public class SortController {
     */
    @FXML
    public void runStep() {
-      playAuto = false;
-      sort(true, oppositeDirection);
+      if (!sortComplete) {
+         resetAnimation();
+
+         sort(oppositeDirection, false);
+      }
 
    }
 
@@ -180,11 +368,9 @@ public class SortController {
     */
    @FXML
    public void runAuto() {
-      playAuto = true;
-      try {
-         TIMER.scheduleAtFixedRate(AUTO, 0, 100);
-      } catch (IllegalStateException e) {
-      } // Just means the task is already scheduled
+      playAni = true;
+
+      updateAutoSort();
    }
 
    /**
@@ -192,20 +378,9 @@ public class SortController {
     */
    @FXML
    public void runReset() {
-      System.out.println(true);
-      array = Arrays.copyOf(defaultArray, defaultArray.length);
+      reset();
 
-      grid.getChildren().clear();
-
-      for (int i = 0; i < array.length; i++) {
-         Rectangle rect = array[i];
-
-         grid.getChildren().add(rect);
-         rect.setX(rect.getWidth() * i);
-      }
-      
-      iSort = 0;
-      jSort = 0;
+      displayArray();
    }
 
    /**
@@ -217,11 +392,13 @@ public class SortController {
    }
 
    /**
-    * This method will reverse a step.
+    * This sort the entire array in an istant.
     */
    @FXML
-   public void runReverseStep() {
-
+   public void runFullSort() {
+      while (!sortComplete) {
+         sort(oppositeDirection, true);
+      }
    }
 
    /**
@@ -229,6 +406,118 @@ public class SortController {
     */
    @FXML
    public void runRandomize() {
+      reset();
+      double[] arr = App.generateArray(array.length, grid.getHeight());
 
+      // Pretty much does the same thing in the initialize but instead it creates it's
+      // own array
+      for (int i = 0; i < array.length; i++) {
+
+         Rectangle rect = new Rectangle(App.getRectWidth(), 0);
+         rect.setStroke(Color.BLACK);
+         rect.setStrokeWidth(App.getRectWidth() / 5);
+
+         grid.getChildren().add(rect);
+
+         rect.setX(App.getRectWidth() * i);
+
+         double height = arr[i];
+
+         if (height < 0.2)
+            height += 0.3;
+
+         // Set color, height and opacity
+         rect.setFill(Color.GREY);
+         rect.setHeight(height);
+         rect.setOpacity(0);
+
+         // Subtle fade in animation
+         FadeTransition fiTransition = new FadeTransition(Duration.seconds(1.5), rect);
+         fiTransition.setFromValue(0.0);
+         fiTransition.setToValue((i > 1) ? inactiveOpacity : 1.0);
+         fiTransition.play();
+
+         array[i] = rect;
+      }
+
+      defaultArray = Arrays.copyOf(array, array.length);
+   }
+
+   /**
+    * This method will reset values
+    */
+   private void reset() {
+      resetAnimation();
+      reminaingComparisons = defaultRemainingComparisons;
+
+      sortComplete = false;
+
+      array = Arrays.copyOf(defaultArray, defaultArray.length);
+
+      iSort = 0;
+      jSort = 1;
+
+      img_iArrow.setX(-img_iArrow.getFitWidth() / 3);
+      img_jArrow.setX(-img_jArrow.getFitWidth() / 3);
+
+      img_iArrow.setX(-img_iArrow.getFitWidth() / 3);
+      img_jArrow.setX(-img_iArrow.getFitWidth() / 3);
+
+      grid.getChildren().clear();
+
+      lbl_rc.setText("Remaining Comparisons: " + reminaingComparisons);
+      lbl_mr.setText("Main Rectangle: 0");
+      lbl_sr.setText("Sub Rectangle: 1");
+      lbl_pc.setText("Positive Comparison: False");
+      lbl_sc.setText("Sort Complete: False");
+   }
+
+   /**
+    * This method will display the array to the screen.
+    */
+   private void displayArray() {
+      for (int i = 0; i < array.length; i++) {
+         Rectangle rect = array[i];
+
+         grid.getChildren().add(rect);
+         rect.setX(rect.getWidth() * i);
+
+         rect.setFill(Color.GREY);
+         rect.setOpacity((i > 1) ? inactiveOpacity : 1.0);
+      }
+   }
+
+   /**
+    * This method will play a scene animation and return to the home screen.
+    * 
+    * @throws IOException Will throw an error if the FXML file is unable to load.
+    */
+   @FXML
+   void switchToHome() throws IOException {
+      Parent nextScene = App.loadFXML("home");
+      sp_root.getChildren().add(nextScene);
+      nextScene.translateYProperty().set(-sp_root.getScene().getHeight());
+
+      // Create a timeline
+      Timeline timeline = new Timeline(
+            new KeyFrame(Duration.seconds(1), new KeyValue(nextScene.translateYProperty(), 0, Interpolator.EASE_BOTH)),
+            new KeyFrame(Duration.seconds(1),
+                  new KeyValue(hb_container.translateYProperty(), sp_root.getScene().getHeight(), Interpolator.EASE_BOTH)));
+      timeline.play();
+
+      // Start animation
+      timeline.setOnFinished((e) -> {
+         App.setArray(null);
+         sp_root.getChildren().remove(hb_container);
+         sp_root.getChildren().remove(nextScene);
+
+         try {
+            App.setRoot("home");
+         } catch (IOException err) {
+            err.printStackTrace();
+         }
+      });
+
+      timeline.play();
    }
 }
